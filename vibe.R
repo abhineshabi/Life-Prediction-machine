57a4c92f9b330befb4a095e04d237f5058af6607
Abhinesh <abhinesh59@gmail.com>
2018-06-27 19:48:36 +0530

    Add files via upload

--- vibe.R
+++ vibe.R
@@ -1,3 +1,5 @@
+library(ggplot2)
+
 mo=read.csv(choose.files())
 key=data.frame(val=800)
 
@@ -8,3 +10,26 @@ predict(fit)
 newcar = data.frame(val=450)
 predict(fit, newcar)
 ggplot(mo,mapping = aes(mo$val,mo$temp))+geom_point()+geom_smooth(method="lm")
+
+
+
+
+mo=read.csv(choose.files())
+key=data.frame(val=800)
+
+fit=lm(temp ~ val,mo)
+predict(fit,key)
+ki=subset.data.frame(mo$sensorReading>800)
+predict(fit)
+newcar = data.frame(sensorReading=450)
+predict(fit, newcar)
+require(ggplot2)
+ggplot(mo,mapping = aes(mo$sensorReading,mo$temp))+geom_point()+geom_smooth(method="lm")
+
+
+
+
+##for multiple linear Regression
+mfit = lm(sensorReading ~ Time + temp + hum, data=mo)
+mfit = lm(mpg ~ wt + disp + cyl, data=mtcars)
+ggplot(mo, aes(x=mo$Time, y=mo$sensorReading, col=mo$temp, size=mo$hum)) + geom_point()
